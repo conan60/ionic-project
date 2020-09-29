626b32858caa1d27d763f25066803d9510462559
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-09-29 20:29:47 +0100

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CryptoCoin } from '../models/crypto-coin';
+import { CoingeckoConsumerService } from '../services/coingecko-consumer.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  bitcoin$: CryptoCoin;
+  constructor(private coinProvider: CoingeckoConsumerService) {}
+
+  async ngOnInit(){
+    let d = await this.changeFiat();
+    this.bitcoin$ = d[0];
+    console.log('bit', this.bitcoin$);
+  }
+
+  changeFiat(){
+    return this.coinProvider.getCoinInfo('usd', 'bitcoin').toPromise();
+  }
+
 
 }
